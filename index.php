30ae6a9015520fea66c74608b43d923c9d7ac36e
pmpr.dev <pmpr.dev@gmail.com>
2024-10-14 23:04:56 +0000

    Obfuscated

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 /*   _______________________________________
     |  Obfuscated by PMPR - Php Obfuscator  |
-    |             670da31ba326d             |
+    |             670da3740d446             |
     |_______________________________________|
 */
  use Pmpr\Module\DomainManager\DomainManager; DomainManager::symcgieuakksimmu();
