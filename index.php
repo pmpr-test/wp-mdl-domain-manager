4e8bf08829cb87fee012908f091dbe72bd86fa93
pmpr.dev <pmpr.dev@gmail.com>
2024-10-14 16:29:30 +0000

    Obfuscated

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 /*   _______________________________________
     |  Obfuscated by PMPR - Php Obfuscator  |
-    |             670d00143911d             |
+    |             670d46c248da1             |
     |_______________________________________|
 */
  use Pmpr\Module\DomainManager\DomainManager; DomainManager::symcgieuakksimmu();
--- src/Pmpr/Module/DomainManager/REST/REST.php
+++ src/Pmpr/Module/DomainManager/REST/REST.php
@@ -1,7 +1,7 @@
 <?php
 /*   _______________________________________
     |  Obfuscated by PMPR - Php Obfuscator  |
-    |             670d00143911d             |
+    |             670d46c248da1             |
     |_______________________________________|
 */
  namespace Pmpr\Module\DomainManager\REST; use Pmpr\Common\Foundation\REST\RESTRegister; class REST extends RESTRegister { public function gigwcakmiyayoigw() { $this->ogyceaekywowkqsc(Controller::symcgieuakksimmu()); } }
